482a7c92972e3bf6ddf6f4163b0d58628e8d0dab
ZeyadHaridy <es-zyad.ismail2027@alexu.edu.eg>
2024-12-04 10:45:37 +0200

    Add profile manager and authenticator

---
+++ main/java/authenticator/Validator.java
@@ -0,0 +1,36 @@
+package authenticator;
+
+public class Validator {
+
+    // Method to validate a five alpha-numeric ID
+    public static boolean isValidID (String ID) {
+        String regex = "^\\w+$";
+        return ID.matches(regex);
+    }
+
+    // Method to validate ID (T for Trainer, M for Member, C for Class)
+    public static boolean isValidID (String id, char specialChar) {
+        String regex = "^" + specialChar + "\\d+$";
+        return id.matches(regex);
+    }
+
+    // Method to validate name
+    public static boolean isValidName (String name) {
+        String regex = "^[a-zA-Z]{2,}(\\s[a-zA-Z]{2,})*$";
+        return name.matches(regex);
+    } 
+
+    // Method to validate email address
+    public static boolean isValidEmail (String email) {
+        String regex = "^[\\w.-]+@\\w+\\.(com|co\\.uk|co\\.in|ru)$"; 
+        return email.matches(regex);
+    }
+
+    // Method to validate phone number --> (+20 0123456789 OR +20123456789 OR 0123456789)
+    public static boolean isValidPhoneNumber (String phoneNumber) {
+        String regex = "^(\\+20\\s?(10|11|12|15)\\d{8}|0(10|11|12|15)\\d{8})$";
+        return phoneNumber.matches(regex);
+    }    
+
+}
+
---
+++ main/java/profilemanagement/backend/Profile.java
@@ -0,0 +1,31 @@
+package profilemanagement.backend;
+
+public class Profile {
+    public String profilePhotoPath;
+    public String coverPhotoPath;
+    public String bio;
+
+    public String getProfilePhotoPath() {
+        return profilePhotoPath;
+    }
+
+    public void setProfilePhotoPath(String profilePhotoPath) {
+        this.profilePhotoPath = profilePhotoPath;
+    }
+
+    public String getCoverPhotoPath() {
+        return coverPhotoPath;
+    }
+
+    public void setCoverPhotoPath(String coverPhotoPath) {
+        this.coverPhotoPath = coverPhotoPath;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
